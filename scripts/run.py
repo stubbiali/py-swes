8bbd3a4f7fa5e1bae8b58f525175516e9d807123
Stefano Ubbiali <subbiali@phys.ethz.ch>
2023-02-28 14:03:45 +0100

    Use click.

--- scripts/run.py
+++ scripts/run.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
+import click
+
 from swes.config import Config
 from swes.driver import Driver
 
 
-def main():
-    config = Config.from_yaml("../config/williamson_1.yml")
+@click.command()
+@click.option("-c", "--config-file", type=str, help="Path to the configuration file.")
+def main(config_file: str) -> None:
+    config = Config.from_yaml(config_file)
     driver = Driver(config)
     driver.run()
 
