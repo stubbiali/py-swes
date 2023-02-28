05a20785c3f60ceb1d167274f07faa77cf6fc702
Stefano Ubbiali <subbiali@phys.ethz.ch>
2023-02-28 15:26:05 +0100

    Add license header.

--- src/swes/diffusion.py
+++ src/swes/diffusion.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+#
+# py-swes: A Python solver for the Shallow Water Equations over a Sphere (SWES)
+# Copyright (C) 2016-2023, ETH Zurich
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import annotations
 from gt4py.cartesian import gtscript
 from typing import TYPE_CHECKING
@@ -134,7 +151,7 @@ class Diffusion:
             dt=dt,
             nu=self.nu,
             origin=(state.grid.hx, state.grid.hy + 1, 0),
-            domain=(state.grid.nx, state.grid.ny - 2, 1)
+            domain=(state.grid.nx, state.grid.ny - 2, 1),
         )
 
 
